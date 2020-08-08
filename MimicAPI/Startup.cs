1f9adea1db287b4e6e83c40367d43275e85aa144
joseednaldo <49928456+joseednaldo@users.noreply.github.com>
2020-08-08 15:34:41 -0300

    Criando meu contexto de banco

--- MimicAPI/Startup.cs
+++ MimicAPI/Startup.cs
@@ -5,8 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MimicAPI.Database;
 
 namespace MimicAPI
 {
@@ -16,6 +19,14 @@ namespace MimicAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //add mvc como serviço
+            services.AddDbContext<MimicContext>(opt =>{
+                //Database  = nossa pasta criada "Database"
+                opt.UseSqlite("Data Source=Database\\Mimic.db");
+            });
+            services.AddMvc();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +37,10 @@ namespace MimicAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+           // app.UseRouting();
+           
+            app.UseMvc();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
